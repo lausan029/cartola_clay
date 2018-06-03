610a3d30edaf024f067afab6bd90f78c830a4e29
Laura Sanchez <lau.san029@gmail.com>
2018-06-03 12:52:01 -0300

    Ajuste validación caso borde

--- app/controllers/cartola_controller.rb
+++ app/controllers/cartola_controller.rb
@@ -15,10 +15,14 @@ class CartolaController < ApplicationController
   def import
     begin
       movements = Roo::Excelx.new(params[:file].path, file_warning: :ignore)
-      #Roo::Spreadsheet.open(params[:file], extension: :xlsx)
       export_file_path = [Rails.root, "public", "cartola_correcta.xls"].join("/")
         if File.file?(export_file_path)
-          validate_movement(movements)
+          movements_old = Roo::Spreadsheet.open(export_file_path, extension: :xls)
+            if movements.last_row < movements_old.last_row
+              especial_movement(movements,movements_old)
+            else
+              validate_movement(movements)
+            end
         else
           create_movement(movements)
         end
@@ -29,10 +33,60 @@ class CartolaController < ApplicationController
     end
   end 
 
-  # Summary: Validacion de cartola
+  # Summary: Validación de cartola cuando es cambio de mes
   #
-  # Params:
-  # Return:  
+  # Params: Archivo precesado anteriormente
+  # Return: status
+  def especial_movement movements,movements_old
+    begin
+      book = Spreadsheet::Workbook.new
+      sheet1 = book.create_worksheet :name => 'cartola_correcta'
+      sheet1.row(0).replace movements_old.row(1)
+      count = 0
+        #Llena sheet con archivo viejo
+        (2..movements_old.last_row).each do |i|
+          count = count + 1
+          sheet1.row(count).replace movements_old.row(i)
+        end
+        #remplazo repetido viejos por actualizados
+        (2..movements.last_row).each do |rw|
+          (2..movements_old.last_row).each do |r|
+            if (movements.row(rw).last == movements_old.row(r).last) and (movements.row(rw).first != movements_old.row(r).first)
+              sheet1.row(r).replace movements.row(rw)
+            end
+          end
+        end
+        #Inserta resto de archivo nuevo
+        (2..movements.last_row).each do |rw|
+          unless movements.row(rw).presence_in sheet1
+            count = sheet1.row_count + 1
+            sheet1.row(count).replace movements.row(rw)
+          else
+            puts "existe"
+          end
+        end
+        #Quito los rows nil
+        (1..sheet1.row_count).each do |rm|
+          unless sheet1.row(rm).present?
+             sheet1.delete_row(rm)
+          end
+        end
+
+      export_file_path = [Rails.root, "public", "cartola_correcta.xls"].join("/")
+      book.write export_file_path
+      send_file export_file_path, :content_type => "application/vnd.ms-excel", :disposition => 'inline'
+
+      return render json: {status: true}
+    rescue Exception => e
+      puts e
+      return render json: {status: false}
+    end
+  end
+
+  # Summary: Validación de cartola
+  #
+  # Params: Archivo precesado anteriormente
+  # Return: status
   def validate_movement movements
     begin  
       export_file_path = [Rails.root, "public", "cartola_correcta.xls"].join("/")
@@ -41,6 +95,7 @@ class CartolaController < ApplicationController
       book = Spreadsheet::Workbook.new
       sheet1 = book.create_worksheet :name => 'cartola_correcta'
       count = 0
+
       (1..movements.last_row).each do |i|
         (1..movements_old.last_row).each do |r|
           (1..movements.last_row).each do |rw|
@@ -60,7 +115,7 @@ class CartolaController < ApplicationController
 
       export_file_path = [Rails.root, "public", "cartola_correcta.xls"].join("/")
       book.write export_file_path
-      #send_file export_file_path, :content_type => "application/vnd.ms-excel", :disposition => 'inline'
+      send_file export_file_path, :content_type => "application/vnd.ms-excel", :disposition => 'inline'
 
       return render json: {status: true}
     rescue Exception => e
@@ -69,10 +124,10 @@ class CartolaController < ApplicationController
     end
   end
 
-  # Summary: Creacion de cartola correcta
+  # Summary: Creación de primera cartola
   #
-  # Params:
-  # Return:
+  # Params: Archivo a precesar
+  # Return: status
   def create_movement movements
     begin
       header = movements.row(1)
